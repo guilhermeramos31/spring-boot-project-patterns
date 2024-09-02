e5e22847212a9e068a69253cc0c62bfc0d80d2d3
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2024-09-02 17:18:55 -0300

    feat: address and Client mapper`s.

--- src/main/java/com/example/projectpatterns/model/mapper/AddressMapper.java
+++ src/main/java/com/example/projectpatterns/model/mapper/AddressMapper.java
@@ -1,4 +1,27 @@
 package com.example.projectpatterns.model.mapper;
 
+import com.example.projectpatterns.model.Address;
+import com.example.projectpatterns.model.dto.AddressResponse;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class AddressMapper {
+    private final ModelMapper modelMapper;
+
+    public AddressResponse toDTO(Address address) {
+        return modelMapper.map(address, AddressResponse.class);
+    }
+
+    public Address toModel(AddressResponse addressResponse) {
+        return modelMapper.map(addressResponse, Address.class);
+    }
+
+    public List<AddressResponse> toDTO(List<Address> addresses) {
+        return addresses.stream().map(address -> modelMapper.map(address, AddressResponse.class) ).toList();
+    }
 }
--- src/main/java/com/example/projectpatterns/model/mapper/ClientMapper.java
+++ src/main/java/com/example/projectpatterns/model/mapper/ClientMapper.java
@@ -1,4 +1,27 @@
 package com.example.projectpatterns.model.mapper;
 
+import com.example.projectpatterns.model.Client;
+import com.example.projectpatterns.model.dto.ClientResponse;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class ClientMapper {
+    private final ModelMapper modelMapper;
+
+    public ClientResponse toDTO(Client client) {
+        return modelMapper.map(client, ClientResponse.class);
+    }
+
+    public Client toModel(ClientResponse clientResponse) {
+        return modelMapper.map(clientResponse, Client.class);
+    }
+
+    public List<ClientResponse> toDTO(List<Client> clients) {
+        return clients.stream().map( client ->  modelMapper.map(client, ClientResponse.class)).toList();
+    }
 }
