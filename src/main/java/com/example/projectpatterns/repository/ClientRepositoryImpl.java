85997538ea07542c2cdf83a39246082b62599d42
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2024-09-02 19:53:49 -0300

    feat: CRUD methods by Client

--- src/main/java/com/example/projectpatterns/repository/ClientRepositoryImpl.java
+++ src/main/java/com/example/projectpatterns/repository/ClientRepositoryImpl.java
@@ -1,4 +1,37 @@
 package com.example.projectpatterns.repository;
 
-public class ClientRepositoryImpl {
+import com.example.projectpatterns.model.Client;
+import com.example.projectpatterns.repository.interfaces.ClientRepository;
+import com.example.projectpatterns.repository.interfaces.ClientRepositoryContext;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class ClientRepositoryImpl implements ClientRepository {
+    private final ClientRepositoryContext repository;
+
+    @Override
+    public void delete(UUID id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void save(Client client) {
+        repository.save(client);
+    }
+
+    @Override
+    public Client findById(UUID id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Client not found"));
+    }
 }
--- src/main/java/com/example/projectpatterns/repository/interfaces/ClientRepository.java
+++ src/main/java/com/example/projectpatterns/repository/interfaces/ClientRepository.java
@@ -1,4 +1,13 @@
 package com.example.projectpatterns.repository.interfaces;
 
+import com.example.projectpatterns.model.Client;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ClientRepository {
+    void delete(UUID id);
+    List<Client> findAll();
+    void save(Client client);
+    Client findById(UUID id);
 }
