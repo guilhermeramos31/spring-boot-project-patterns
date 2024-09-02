83f17eb2a8d1ea99fa87d3f6c5ecd6695e07f204
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2024-09-02 19:46:30 -0300

    feat: CRUD methods by Address

--- src/main/java/com/example/projectpatterns/repository/interfaces/AddressRepository.java
+++ src/main/java/com/example/projectpatterns/repository/interfaces/AddressRepository.java
@@ -1,4 +1,14 @@
 package com.example.projectpatterns.repository.interfaces;
 
+import com.example.projectpatterns.model.Address;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface AddressRepository {
+    void delete(UUID id);
+    List<Address> findAll();
+    Address findById(UUID id);
+    void save(Address address);
+    void update(Address address);
 }
