5f033c59ad5e756a5564c2bc41d9632ed83bb869
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2024-09-02 17:07:39 -0300

    feat: client dto

--- src/main/java/com/example/projectpatterns/model/dto/ClientRequest.java
+++ src/main/java/com/example/projectpatterns/model/dto/ClientRequest.java
@@ -1,4 +1,14 @@
 package com.example.projectpatterns.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientRequest {
+    private String name;
 }
